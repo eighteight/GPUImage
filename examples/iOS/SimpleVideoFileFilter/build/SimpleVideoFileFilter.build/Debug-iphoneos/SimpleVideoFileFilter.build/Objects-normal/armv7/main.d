0dd2a6235dc2615f00e3b4a57332fecb5245de4d
Vladimir Gusev <bo-ba@mail.ru>
2014-06-30 19:34:15 -0400

    Merged with upstream original branch by Brad

---
+++ examples/iOS/SimpleVideoFileFilter/build/SimpleVideoFileFilter.build/Debug-iphoneos/SimpleVideoFileFilter.build/Objects-normal/armv7/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/eight/repos/GPUImage-eight/examples/iOS/SimpleVideoFileFilter/SimpleVideoFileFilter/main.m \
+  /Users/eight/repos/GPUImage-eight/examples/iOS/SimpleVideoFileFilter/SimpleVideoFileFilter/SimpleVideoFileFilterAppDelegate.h
