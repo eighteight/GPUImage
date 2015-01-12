044258a4bee30d07a70b71af0170e90a0dcae81a
Vladimir Gusev <bo-ba@mail.ru>
2015-01-12 00:07:53 -0500

    0.1.30: Old Fart / sdk 8.1

---
+++ examples/iOS/SimplePhotoFilter/build/SimplePhotoFilter.build/Debug-iphoneos/SimplePhotoFilter.build/Objects-normal/armv7/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/eight/repos/GPUImage-eight/examples/iOS/SimplePhotoFilter/SimplePhotoFilter/main.m \
+  /Users/eight/repos/GPUImage-eight/examples/iOS/SimplePhotoFilter/SimplePhotoFilter/PhotoAppDelegate.h
